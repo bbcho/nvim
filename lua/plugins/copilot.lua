9a420fb91a00aa785084d335d944df8e2629dda7
Ben Cho <ben.cho@gmail.com>
2023-04-21 18:16:18 -0600

    init

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  'github/copilot.vim',
+  -- config = function()
+  --   require 'copilot'.setup {
+  --     -- Copilot Setup
+  --     copilot_no_tab_map = true,
+  --     copilot_assume_mapped = true,
+  --     copilot_tab_fallback = "",
+  --   }
+  -- end
+  --
+}
