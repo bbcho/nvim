158dc3a607f6bb8a641b02f1a288a98cc2a19043
Ben Cho <ben.cho@gmail.com>
2023-07-15 22:54:52 -0600

    jupyter working

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,3 +1,4 @@
+-- keymaps start at line 172
 local wk = require("which-key")
 
 P = function(x)
@@ -29,6 +30,147 @@ local imap = function(key, effect)
 end
 
 local map = vim.api.nvim_set_keymap
+
+-- functions for REPL and Jupyter
+local CELL_MARKER_COLOR = "#C5C5C5"
+local CELL_MARKER = "^# %%%%"
+local CELL_MARKER_SIGN = "cell_marker_sign"
+
+vim.api.nvim_set_hl(0, "cell_marker_hl", { bg = CELL_MARKER_COLOR })
+vim.fn.sign_define(CELL_MARKER_SIGN, { linehl = "cell_marker_hl" })
+
+local function highlight_cell_marker(bufnr, line)
+  local sign_name = CELL_MARKER_SIGN
+  local sign_text = "%%"
+  vim.fn.sign_place(line, CELL_MARKER_SIGN, sign_name, bufnr, {
+    lnum = line,
+    priority = 10,
+    text = sign_text,
+  })
+end
+
+local function show_cell_markers()
+  local bufnr = vim.api.nvim_get_current_buf()
+  vim.fn.sign_unplace(CELL_MARKER_SIGN, { buffer = bufnr })
+  local total_lines = vim.api.nvim_buf_line_count(bufnr)
+  for line = 1, total_lines do
+    local line_content = vim.api.nvim_buf_get_lines(bufnr, line - 1, line, false)[1]
+    if line_content ~= "" and line_content:find(CELL_MARKER) then
+      highlight_cell_marker(bufnr, line)
+    end
+  end
+end
+
+local function show_cell_marker()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+  local line_content = vim.api.nvim_buf_get_lines(bufnr, line - 1, line, false)[1]
+  if line_content ~= "" and line_content:find(CELL_MARKER) then
+    highlight_cell_marker(bufnr, line)
+  else
+    vim.fn.sign_unplace(CELL_MARKER_SIGN, { buffer = bufnr, id = line })
+  end
+end
+
+local function select_cell()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local current_row = vim.api.nvim_win_get_cursor(0)[1]
+  local current_col = vim.api.nvim_win_get_cursor(0)[2]
+
+  local start_line = nil
+  local end_line = nil
+
+  for line = current_row, 1, -1 do
+    local line_content = vim.api.nvim_buf_get_lines(bufnr, line - 1, line, false)[1]
+    if line_content:find(CELL_MARKER) then
+      start_line = line
+      break
+    end
+  end
+  local line_count = vim.api.nvim_buf_line_count(bufnr)
+  for line = current_row + 1, line_count do
+    local line_content = vim.api.nvim_buf_get_lines(bufnr, line - 1, line, false)[1]
+    if line_content:find(CELL_MARKER) then
+      end_line = line
+      break
+    end
+  end
+
+  if not start_line then
+    start_line = 1
+  end
+  if not end_line then
+    end_line = line_count
+  end
+  return current_row, current_col, start_line, end_line
+end
+
+local function execute_cell()
+  local current_row, current_col, start_line, end_line = select_cell()
+  if start_line and end_line then
+    vim.fn.setpos("'<", { 0, start_line + 1, 0, 0 })
+    vim.fn.setpos("'>", { 0, end_line - 1, 0, 0 })
+    require("iron.core").visual_send()
+    vim.api.nvim_win_set_cursor(0, { current_row, current_col })
+  end
+end
+
+local function delete_cell()
+  local _, _, start_line, end_line = select_cell()
+  if start_line and end_line then
+    local rows_to_select = end_line - start_line - 1
+    vim.api.nvim_win_set_cursor(0, { start_line, 0 })
+    vim.cmd("normal!V " .. rows_to_select .. "j")
+    vim.cmd "normal!d"
+    vim.cmd "normal!k"
+  end
+end
+
+local function navigate_cell(up)
+  local is_up = up or false
+  local _, _, start_line, end_line = select_cell()
+  if is_up and start_line ~= 1 then
+    vim.api.nvim_win_set_cursor(0, { start_line - 1, 0 })
+  elseif end_line then
+    local bufnr = vim.api.nvim_get_current_buf()
+    local line_count = vim.api.nvim_buf_line_count(bufnr)
+    if end_line ~= line_count then
+      vim.api.nvim_win_set_cursor(0, { end_line + 1, 0 })
+      _, _, start_line, end_line = select_cell()
+      vim.api.nvim_win_set_cursor(0, { end_line - 1, 0 })
+    end
+  end
+end
+
+local function insert_cell(content)
+  local _, _, _, end_line = select_cell()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local line = end_line
+  if end_line ~= 1 then
+    line = end_line - 1
+    vim.api.nvim_win_set_cursor(0, { end_line - 1, 0 })
+  else
+    line = end_line
+    vim.api.nvim_win_set_cursor(0, { end_line, 0 })
+  end
+
+  vim.cmd "normal!2o"
+  vim.api.nvim_buf_set_lines(bufnr, line, line + 1, false, { content })
+  local current_line = vim.api.nvim_win_get_cursor(0)[1]
+  highlight_cell_marker(bufnr, current_line - 1)
+  vim.cmd "normal!2o"
+  vim.cmd "normal!k"
+end
+
+local function insert_code_cell()
+  insert_cell "# %%"
+end
+
+local function insert_markdown_cell()
+  insert_cell "# %% [markdown]"
+end
+
+-- functions for REPL and Jupyter end
 
 -- save with ctrl+s
 imap("<C-s>", "<cmd>:w<cr><esc>")
@@ -190,6 +332,37 @@ wk.register(
       p = { ":lua require'neotest'.jump.prev({ status = 'failed' })<cr>", 'jump to prev failure'},
       u = { ":lua require'neotest'.summary.toggle()<cr>", 'toggle summary tree'},
     },
+    i = {
+      name = 'iron/jupyter',
+
+      -- x = { execute_cell, "Execute Cell" },
+      i = {
+        name = 'cell',
+        c = { insert_code_cell, "Insert Code Cell" },
+        m = { insert_markdown_cell, "Insert Markdown Cell" },
+        d = { delete_cell, "Delete Cell" },
+      },
+      n = { navigate_cell, "Next Cell" },
+      p = { function() navigate_cell(true) end, "Previous Cell" },
+      s = { function() require("iron.core").run_motion("send_motion") end, "Send Motion" },
+      l = { function() require("iron.core").send_line() end, "Send Line" },
+      t = { function() require("iron.core").send_until_cursor() end, "Send Until Cursor" },
+      f = { function() require("iron.core").send_file() end, "Send File" },
+      ["<cr>"] = { function() require("iron.core").send(nil, string.char(13)) end, "ENTER" },
+      I = { function() require("iron.core").send(nil, string.char(03)) end, "Interrupt" },
+      C = { function() require("iron.core").close_repl() end, "Close REPL" },
+      c = { function() require("iron.core").send(nil, string.char(12)) end, "Clear" },
+      m = { 
+        name = 'mark',
+        s = { function() require("iron.core").send_mark() end, "Send Mark" },
+        m = { function() require("iron.core").run_motion("mark_motion") end, "Mark Motion" },
+        r = { function() require("iron.marks").drop_last() end, "Remove Mark" },
+      },
+      R = { "<cmd>IronRepl<cr>", "REPL" },
+      S = { "<cmd>IronRestart<cr>", "Restart" },
+      F = { "<cmd>IronFocus<cr>", "Focus" },
+      H = { "<cmd>IronHide<cr>", "Hide" }
+    },
     p = {
       name = 'explorer',
       v = {vim.cmd.Ex, 'file explorer' },
@@ -312,6 +485,7 @@ wk.register(
   }, { mode = 'n', prefix = '<leader>' }
 )
 
+
 -- normal mode
 wk.register({
   ['<c-LeftMouse>'] = { '<cmd>lua vim.lsp.buf.definition()<CR>', 'go to definition' },
@@ -345,6 +519,15 @@ wk.register({
 wk.register({
   ['<leader>'] = { '<Plug>SlimeRegionSend', 'run code region' },
   ['p'] = { '"_dP', 'replace without overwriting reg' },
+  i = {
+    name = 'iron/jupyter',
+    s = { function() require("iron.core").visual_send() end, "Send" },
+    L = { function() require("iron.marks").clear_hl() end, "Clear Highlight" },
+    m = {
+      name = 'mark',
+      v = { function() require("iron.core").mark_visual() end, "Mark Visual" }, 
+    }
+  },
 }, { mode = 'v', prefix = "<leader>" })
 
 wk.register({
@@ -355,3 +538,4 @@ wk.register({
   ['<cm-i>'] = { '<esc>o```{python}<cr>```<esc>O', "r code chunk" },
   ['<m-I>'] = { '<esc>o```{python}<cr>```<esc>O', "r code chunk" },
 }, { mode = 'i' })
+
