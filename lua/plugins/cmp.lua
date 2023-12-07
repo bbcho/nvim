59b8ef0261b6ec5eebd1783b8f0fd4de560c512a
Ben Cho <ben.cho@gmail.com>
2023-12-06 23:04:25 -0700

    minor cleanup

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -70,7 +70,6 @@ return {
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "path" },
-        }, {
           { name = "buffer" },
         }),
         -- formatting = {
