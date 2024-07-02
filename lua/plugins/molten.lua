c4c10443ff5d2d678f00245361b6cbc54be1e6df
Ben Cho <ben.cho@gmail.com>
2024-07-01 22:48:02 -0600

    molten images only work on native linux

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -4,7 +4,7 @@ return {
     build = ":UpdateRemotePlugins",
     -- version = "^1.0.0",
     -- init = function()
-    -- vim.g.molten_auto_image_popup = true
+    --   -- vim.g.molten_auto_image_popup = true
     --   -- these are examples, not defaults. Please see the readme
     --   vim.g.molten_image_provider = "wezterm"
     --   vim.g.molten_auto_open_output = false
